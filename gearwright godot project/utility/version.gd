f8b043541879c0e8a54e177d7cabf5f9c365d71c
markopolodev <marko.polo.jorgensen@gmail.com>
2025-05-31 20:23:05 -0500

    2.5.7

--- gearwright godot project/utility/version.gd
+++ gearwright godot project/utility/version.gd
@@ -2,7 +2,7 @@ extends Node
 
 # Project Settings -> Application -> Config -> Version
 
-const VERSION = "2.5.6"
+const VERSION = "2.5.7"
 
 func _ready():
 	get_window().title = "Gearwright %s" % VERSION
